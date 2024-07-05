0d0e1dd6d3dc50d6117a220751ff1f449181f56e
mrchysen <mrchysen@mail.ru>
2024-07-05 10:46:32 +0700

    feat: add GeometryComparer for comparer values in geometry

---
+++ GeometryAlgorithms/GeometryComparer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryAlgorithms;
+
+/// <summary>
+/// Класс - сравниватель точек с заданной погрешностью
+/// </summary>
+public class GeometryComparer
+{
+    public const double Eps = 0.0001d;
+
+    public static bool IsEqual(double v1, double v2)
+    {
+        return Math.Abs(v1 - v2) < Eps;
+    }
+}
