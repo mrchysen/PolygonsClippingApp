18c152c21cf479e9246a4db6958c46234e003a55
mrchysen <mrchysen@mail.ru>
2024-07-10 07:56:08 +0700

    feat: change PolygonModel

--- GeometryAlgorithms/Models/PolygonModel.cs
+++ GeometryAlgorithms/Models/PolygonModel.cs
@@ -4,11 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Shapes;
+using System.Windows;
+using System.Windows.Media;
+using System.Text.Json.Serialization;
 
 namespace GeometryAlgorithms.Models;
 
-public  class PolygonModel
+public class PolygonModel
 {
+    [JsonIgnore]
+    public Polygon Polygon = new();
+    [JsonPropertyName("name")]
     public string Name { get; set; }
-    public Polygon Polygon { get; set; }
+    [JsonPropertyName("points")]
+    public IEnumerable<Point> Points {
+        get => Polygon.Points;
+        set => Polygon.Points = new(value);
+    }
+    [JsonPropertyName("fill")]
+    public Brush Fill { 
+        get => Polygon.Fill;
+        set => Polygon.Fill = value; 
+    }
+    [JsonPropertyName("stroke")]
+    public Brush Stroke { 
+        get => Polygon.Stroke; 
+        set => Polygon.Stroke = value;
+    }
+    [JsonPropertyName("stroke_thickness")]
+    public double StrokeThickness { 
+        get => Polygon.StrokeThickness;
+        set => Polygon.StrokeThickness = value;
+    }
 }
